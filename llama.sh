a917f5643764d07386963d61ae65f2b7987c7e22
bryanhpchiang <bryanhpchiang@protonmail.com>
2023-03-12 20:55:53 -0700

    aria2c

--- llama.sh
+++ llama.sh
@@ -8,7 +8,8 @@
 
 PRESIGNED_URL="https://agi.gpt4.org/llama/LLaMA/*"
 
-MODEL_SIZE="7B,13B,30B,65B"  # edit this list with the model sizes you wish to download
+MODEL_SIZE="7B"  # edit this list with the model sizes you wish to download
+# MODEL_SIZE="7B,13B,30B,65B"  # edit this list with the model sizes you wish to download
 TARGET_FOLDER="./"             # where all files should end up
 
 declare -A N_SHARD_DICT
@@ -19,8 +20,8 @@ N_SHARD_DICT["30B"]="3"
 N_SHARD_DICT["65B"]="7"
 
 echo "Downloading tokenizer"
-wget ${PRESIGNED_URL/'*'/"tokenizer.model"} -O ${TARGET_FOLDER}"/tokenizer.model"
-wget ${PRESIGNED_URL/'*'/"tokenizer_checklist.chk"} -O ${TARGET_FOLDER}"/tokenizer_checklist.chk"
+aria2c ${PRESIGNED_URL/'*'/"tokenizer.model"} -o ${TARGET_FOLDER}"/tokenizer.model"
+aria2c ${PRESIGNED_URL/'*'/"tokenizer_checklist.chk"} -o ${TARGET_FOLDER}"/tokenizer_checklist.chk"
 
 (cd ${TARGET_FOLDER} && md5sum -c tokenizer_checklist.chk)
 
@@ -30,10 +31,10 @@ do
     mkdir -p ${TARGET_FOLDER}"/${i}"
     for s in $(seq -f "0%g" 0 ${N_SHARD_DICT[$i]})
     do
-        wget ${PRESIGNED_URL/'*'/"${i}/consolidated.${s}.pth"} -O ${TARGET_FOLDER}"/${i}/consolidated.${s}.pth"
+        aria2c ${PRESIGNED_URL/'*'/"${i}/consolidated.${s}.pth"} -o ${TARGET_FOLDER}"/${i}/consolidated.${s}.pth"
     done
-    wget ${PRESIGNED_URL/'*'/"${i}/params.json"} -O ${TARGET_FOLDER}"/${i}/params.json"
-    wget ${PRESIGNED_URL/'*'/"${i}/checklist.chk"} -O ${TARGET_FOLDER}"/${i}/checklist.chk"
+    aria2c ${PRESIGNED_URL/'*'/"${i}/params.json"} -o ${TARGET_FOLDER}"/${i}/params.json"
+    aria2c ${PRESIGNED_URL/'*'/"${i}/checklist.chk"} -o ${TARGET_FOLDER}"/${i}/checklist.chk"
     echo "Checking checksums"
     (cd ${TARGET_FOLDER}"/${i}" && md5sum -c checklist.chk)
 done
